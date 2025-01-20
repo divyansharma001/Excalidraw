a84ab7049db35e8c2ab0c3c3b11dc8d2f25821c2
Divyansh Sharma <divyanshsharma4507@gmail.com>
2025-01-20 21:38:47 +0530

    Add a basic route to return a Hello World message

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,12 @@
+//this is an express server
 import express from 'express';
 
+import { Request, Response } from 'express';
+
 const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'Hello World!' });
+});
 
 app.listen(3000);
